cec369db855dea271ca2049e8e5786c7767a7240
adidatssukhyar-ux <adidats.sukhyar@gmail.com>
2025-09-11 08:47:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('vibeForm').addEventListener('submit', function(e) {
-  // Prevent default form submission for demo
   e.preventDefault();
   // Hide the form and show thank you message
   this.style.display = 'none';
